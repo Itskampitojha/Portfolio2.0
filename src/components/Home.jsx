f3c1ee8cb96da4bd2fffb7d09a847d5a661e6eb3
Kampit Ojha <97059622+Itskampitojha@users.noreply.github.com>
2023-12-29 21:45:55 +0530

    changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
           Kampit Ojha
         </h1>
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
-          I'm a SOftware Developer.
+          I'm a software developer.
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
           I’m a software developer specializing in building (and occasionally
